ac9c15977377bee4d6d32ecc1c4e3ab495137ece
Jagan0901 <110884222+Jagan0901@users.noreply.github.com>
2024-01-17 11:03:56 +0530

    err rectified

--- index.js
+++ index.js
@@ -18,6 +18,12 @@ const MONGO_URL = process.env.MONGO_URL;
 
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://pizzas-hunt.netlify.app/");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
 
 async function createConnection(){
     const client = new MongoClient(MONGO_URL);
